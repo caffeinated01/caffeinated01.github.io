295d94f6b4af4521e25557d6b8297b7b7a995727
Javier <javier.has.no.life@gmail.com>
2023-04-02 10:42:15 +0800

    theme is cached in browser & minor changes

--- theme.js
+++ theme.js
@@ -1,5 +1,51 @@
-var toggle = document.getElementById("change-theme");
+const toggle = document.getElementById("change-theme");
+const icon = document.getElementById("theme-icon");
 
-toggle.onclick = function (){
-    document.body.classList.toggle("light");
+let darkMode = localStorage.getItem("darkMode");
+
+console.log(darkMode);
+
+const toggleLight = () => {
+  document.body.classList.add("light");
+  localStorage.setItem("darkMode", null);
+  icon.classList.remove("fa-moon");
+  icon.classList.add("fa-sun");
 };
+
+const toggleDark = () => {
+  document.body.classList.remove("light");
+  localStorage.setItem("darkMode", "enabled");
+  icon.classList.remove("fa-sun");
+  icon.classList.add("fa-moon");
+};
+
+if (darkMode === "enabled") {
+  toggleDark();
+} else {
+  toggleLight();
+}
+
+toggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  document.body.classList.toggle("light");
+  toggle.animate(
+    [
+      { transform: "translateY(0)" },
+      { transform: "translateY(20%)" },
+      { transform: "translateY(15%)" },
+      { transform: "translateY(5%)" },
+      { transform: "translateY(0)" },
+    ],
+    {
+      duration: 200,
+    }
+  );
+
+  if (darkMode !== "enabled") {
+    toggleDark();
+    console.log(darkMode);
+  } else {
+    toggleLight();
+    console.log(darkMode);
+  }
+});
